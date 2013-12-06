386fe1c5503e86c71896bfe9bd15df3ecf0a0c3c
v-zubritsky <v.zubritsky@gmail.com>
2013-12-07 00:01:26 +0300

    solution initialized

---
+++ .tests/Optimization.Linear.Tests/Linear_Tests.fs
@@ -0,0 +1,13 @@
+﻿module Linear.Tests
+
+open Linear
+open Xunit
+open FsUnit.Xunit
+
+[<Fact>]
+let ``some test`` () = 
+  5 |> should equal 5
+
+[<Fact>]
+let ``some another test`` () = 
+  5 |> should equal 6
---
+++ Optimization.Linear/Linear.fs
@@ -0,0 +1,8 @@
+﻿module Linear
+
+type plan = float list * int list
+
+let someFunc = 
+  let a = matrix [ [1.0; 7.0];
+                   [2.0; 5.0]; ]
+  a
