298993edf3f2b4107031cd0a1998896ce2a0b7cc
v-zubritsky <v.zubritsky@gmail.com>
2013-12-09 10:10:57 +0300

    solving sample in interactive mode

--- Optimization.Linear/Linear.fs
+++ Optimization.Linear/Linear.fs
@@ -1,8 +1,54 @@
-﻿module Linear
+﻿#if INTERACTIVE
+#r @"bin\Debug\FSharp.PowerPack.dll"
+#endif
 
-type plan = float list * int list
+(*
+  Task:
+    n: int - count of product types.
+    m: int - count of resource types.
+    b: vector - count of recources available.
+    c: vector - profit from products. Length = n.
+    a: matrix - cost of product production. Dimensions = (m,n).
+    x: vector - plan. satisfies all conditions.
+    J: indices - set of indeces for x vector. Length = n.
 
-let someFunc = 
-  let a = matrix [ [1.0; 7.0];
-                   [2.0; 5.0]; ]
-  a
+    c * x -> max
+    x >= 0
+    A * x <= b
+*)
+
+type Matrix<'a> with 
+  member self.Columns(indices: int list) =
+    let indToCol =
+      self.Column 
+        >> Vector.Generic.toArray 
+        >> List.ofArray
+         
+    indices 
+      |> List.map (indToCol) 
+      |> Matrix.Generic.ofList
+end
+
+type Vector<'a> with
+  member self.Items(indices: int list) =
+    indices 
+      |> List.map (Vector.Generic.get self) 
+      |> Vector.Generic.ofList
+end
+
+// Example   
+let A = matrix [[3.0;  1.0; 1.0; 0.0];
+                [1.0; -2.0; 0.0; 1.0]]
+let b = vector [1.0; 1.0]
+let c = vector [1.0; 4.0; 1.0; -1.0]
+let m = 2
+let n = 4
+
+let plan = (vector [0.0; 0.0; 1.0; 1.0;], [2; 3])
+let A'b = A.Columns <| snd plan
+let c'b = c.Items <| snd plan
+
+
+
+  
+ 
